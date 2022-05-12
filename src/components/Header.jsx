0de0251a631442c26e1d0ed00beb856478705661
Clayton Knapp <csjknapp@gmail.com>
2022-05-12 10:07:46 -0700

    imported useListContext into Header, added item number

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from '../App.css';
+import { useListContext } from '../context/ListProvider';
 
 export default function Header() {
+  const { listState } = useListContext();
+
+
   return (
     <header className={styles['header']}>
       <h2>My Shopping List</h2>
-      <h3>Total Items: </h3>
+      <h3>Total Items: {listState.length}</h3>
       <button
       
       >Clear Cart</button>
