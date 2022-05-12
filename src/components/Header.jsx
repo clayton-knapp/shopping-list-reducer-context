5dfefc68cf32ff7a086dd54efe6aa5298cf500eb
Clayton Knapp <csjknapp@gmail.com>
2022-05-12 10:27:20 -0700

    added Left to Purchase count in header as stretch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,17 @@ import { useListContext } from '../context/ListProvider';
 export default function Header() {
   const { listState, clearList } = useListContext();
 
+  // STRETCH we want to find the number of un-purchased items (unchecked)
+  const unpurchasedList = listState.filter((item) => item.purchased === false);
+  console.log('unpurchasedList', unpurchasedList);
 
   return (
     <header className={styles['header']}>
       <h2>My Shopping List</h2>
-      <h3>Total Items: {listState.length}</h3>
+      <div>
+        <h4>Items in Cart: {listState.length}</h4>
+        <h4>Left to Purchase: {unpurchasedList.length}</h4>
+      </div>
       <button
         onClick={() => {
           window.confirm('Are you sure you wish to clear your cart?') ? clearList() : onCancel("cancel")
