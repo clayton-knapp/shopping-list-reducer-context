d4746ba8d0fb20d43d515f714eb5e0eb56f1d513
Clayton Knapp <csjknapp@gmail.com>
2022-05-11 18:01:34 -0700

    added conditional rendering ternary to render input form upon edit and back when save button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import styles from '../App.css';
+import { useState } from 'react';
 
 export default function Item({ item, handleDeleteItem }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedItem, setUpdatedItem] = useState('');
+
   return (
     <div className={styles['item']}>
-      <p>{item.item}</p>
-      <button
-        name='edit'
-      >Edit</button>
+      {isEditing
+        ? <>
+          <form action="">
+            <input
+              type="text"
+              placeholder='Edit item'
+              value={updatedItem}
+              onChange={(e) => setUpdatedItem(e.target.value)}
+            />
+            <button
+              onSubmit={() => {
+                setIsEditing(false);
+              }}
+            >Save</button>
+          </form>  
+        </>
+        : <>
+            <p>{item.item}</p>
+            <button
+              name='edit'
+              onClick={() => {
+                setIsEditing(true);
+                }}
+            >Edit</button>
+          </>
+      }
+    
       <button
         name='delete'
         onClick={() => handleDeleteItem(item.id)}
